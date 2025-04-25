f4e86a948e70c0e618509ee7ee474055bd60b9ac
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-24 21:49:44 -0300

    feat: Configure Express server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 
-function sum(a: number, b: number): number {
-  return a + b;
-}
+const PORT = 3333;
 
-const result: number = sum(5, 3);
-console.log(`Resultado da soma ${result}`);
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`));

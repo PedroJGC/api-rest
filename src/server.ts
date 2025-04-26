a8a8f1982b54f74230401f8a5383d3cda0bc683b
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-25 21:32:50 -0300

    feat: Data in the body of the POST request

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,18 @@ import express from 'express';
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
 
 app.get('/products', (request, response) => {
-  // /products?page=1&limit=10
   const { page, limit } = request.query;
 
   response.send(`Página ${page} de ${limit}`);
+});
+
+app.post('/products', (request, response) => {
+  const { name, price } = request.body;
+
+  response.send(`Produto ${name} custa ${price}`);
 });
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));

9852af359b8712e3758999310ccc837d24f589eb
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-30 18:16:07 -0300

    feat: Controller

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,21 +1,14 @@
 import { Router } from 'express';
 import { myMiddleware } from '../middleware/my-middleware';
+import { ProductsController } from '../controllers/ProductsController';
 
 const productsRoutes = Router();
+const productsController = new ProductsController();
 
-productsRoutes.get('/:id', (request, response) => {
-  const { page, limit } = request.query;
-  const { id } = request.params;
-
-  response.send(`Página ${page} de ${limit} - ID: ${id}`);
-});
+productsRoutes.get('/', productsController.index);
 
 // Middleware local em uma rota especifica.
-productsRoutes.post('/', myMiddleware, (request, response) => {
-  const { name, price } = request.body;
-
-  response.json({ name, price, user_id: request.user_id });
-});
+productsRoutes.post('/', myMiddleware, productsController.create);
 
 // Exportação mais explícita
 export { productsRoutes };
